32a57894597b994e51c0ea06224e7a21b3c45fc8
qmei1123 <qmei@bu.edu>
2018-03-17 09:16:54 -0400

    add inner shape

--- sketch.js
+++ sketch.js
@@ -4,26 +4,40 @@ function setup() {
 }
 
 function draw() { 
-    background(209,239,252);
+    background(218,93,120);
     //line following mouse
-        line(mouseX, mouseY, mouseX*20, mouseY*60);
+    
 
     
-    for (i = 0; i < width; i+=400) {
-        for (j = 0; j < height; j+=400) {
-    
+    for (i = 0; i < width; i+=100) {
+        for (j = 0; j < height; j+=100) {
+//    line(mouseX/50 - 10 + i, mouseY/50 - 10 + j, mouseX/100 + 10 +i, mouseY/60 + 10 + j);
+     //       line = map(mouseX, 0 width, i)
     //custome shape
+            
     beginShape();
     fill(255,109,109);
-    strokeWeight(5);
-    stroke(255,109,109);
+    noStroke();
 
-    let vertices = map(mouseX, 0, width, i, j);
+    let verticesTwo = map(mouseY, 0, height, 4, 100);
+
+    for(let b = 0; b < 360; b += verticesTwo) {
+        let x2 = 60 * cos(b) + 180;
+        let y2 = 60 * sin(b) + 180;
+        vertex (x2 + i - 80, y2 + j - 80);
+    }
+    endShape(CLOSE);
+            
+              beginShape();
+    fill(218,93,120);
+    noStroke();
+
+    let vertices = map(mouseX, 0, width, 4, 100);
 
     for(let a = 0; a < 360; a += vertices) {
-        let x = 150 * sin(a) + 180;
-        let y = 150 * cos(a) + 180;
-        vertex (x, y);
+        let x = 30 * sin(a) + 180;
+        let y = 30 * cos(a) + 180;
+        vertex (x + i - 80, y + j - 80);
     }
     endShape(CLOSE);
         }
